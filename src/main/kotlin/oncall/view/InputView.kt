62198fbe6fbe92da7a7e948cda7c89157880bcbf
박지민 <pjm5489@naver.com>
2024-11-18 02:05:25 +0900

    refactor(inputview): class에서 object로 변경

--- src/main/kotlin/oncall/view/InputView.kt
+++ src/main/kotlin/oncall/view/InputView.kt
@@ -2,7 +2,7 @@ package oncall.view
 
 import camp.nextstep.edu.missionutils.Console
 
-class InputView {
+object InputView {
     fun inputMonthAndStartDayMessage(): String {
         println("비상 근무를 배정할 월과 시작 요일을 입력하세요>")
        return Console.readLine()
