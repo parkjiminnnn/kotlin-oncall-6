dedc79f8afb55fa6f6bd67af880ac924ed3eee83
박지민 <pjm5489@naver.com>
2024-11-18 02:05:46 +0900

    feat(sevendays): enum class 정의

---
+++ src/main/kotlin/oncall/view/SevenDays.kt
@@ -0,0 +1,11 @@
+package oncall.view
+
+enum class SevenDays(val day: String) {
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토"),
+    SUNDAY("일")
+}
