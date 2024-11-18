78a9e1f776864d7d4cf31287dc17d6fc233acebe
박지민 <pjm5489@naver.com>
2024-11-18 20:19:53 +0900

    feat(calendar): 주말을 구하는 기능 추가

--- src/main/kotlin/oncall/domain/Calendar.kt
+++ src/main/kotlin/oncall/domain/Calendar.kt
@@ -1,11 +1,13 @@
 package oncall.domain
 
+import oncall.utils.SevenDays
+
 class Calendar(rawMonthAndStartDay: String) {
     private val monthAndStartDay = rawMonthAndStartDay.split(',')
-     val month = monthAndStartDay[0].toInt()
-     val startDay = monthAndStartDay[1]
+    val month = monthAndStartDay[0].toInt()
+    val startDay = monthAndStartDay[1]
 
-    fun days(): Int {
+    fun getUntilDays(): Int {
         return when (month) {
             1, 3, 5, 7, 8, 10, 12 -> 31
             4, 6, 9, 11 -> 30
@@ -13,7 +15,7 @@ class Calendar(rawMonthAndStartDay: String) {
         }
     }
 
-    fun holidays(): List<Int> {
+    fun getPublicHolidays(): List<Int> {
         val holidays = mutableListOf<Int>()
         when (month) {
             1, 3 -> holidays.add(1)
@@ -28,5 +30,32 @@ class Calendar(rawMonthAndStartDay: String) {
             12 -> holidays.add(25)
         }
         return holidays
+    }
+
+    private fun getWeekends(): List<Int> {
+        val sevenDays = SevenDays.entries.toList()
+        var saturdayIndex = sevenDays.size - (sevenDays.indexOfFirst { it.day == startDay } + 1)
+        val weekend = mutableListOf<Int>()
+        while (true) {
+            if (saturdayIndex > getUntilDays()) break
+            weekend.add(saturdayIndex)
+            val sundayIndex = saturdayIndex + 1
+            weekend.add(sundayIndex)
+            saturdayIndex += 7
+        }
+        return weekend
+    }
+
+    fun getFullHolidays(): List<Int> {
+        val fullHolidays = mutableSetOf<Int>()
+        val publicHolidays = getPublicHolidays()
+        val weekends = getWeekends()
+        publicHolidays.forEach { publicHoliday ->
+            fullHolidays.add(publicHoliday)
+        }
+        weekends.forEach { weekend ->
+            fullHolidays.add(weekend)
+        }
+        return fullHolidays.toList().sorted()
     }
 }
