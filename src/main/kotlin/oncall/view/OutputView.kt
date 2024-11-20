ec1b9f759e0a6ac1f2c2a411a59d10fd9a4e5118
박지민 <pjm5489@naver.com>
2024-11-20 18:37:48 +0900

    feat(outputview): 근무자 이름 출력 기능 추ㅏ

--- src/main/kotlin/oncall/view/OutputView.kt
+++ src/main/kotlin/oncall/view/OutputView.kt
@@ -1,24 +1,35 @@
 package oncall.view
 
+import oncall.utils.SevenDays
+
 object OutputView {
-    fun scheduleView(month: Int, days: Int, startDay: String, holidays: List<Int>) {
+    fun scheduleView(month: Int, days: Int, startDay: String, holidays: List<Int>, totalWorkers: List<String>) {
         val sevenDays = SevenDays.entries.toList()
         val startIndex = sevenDays.indexOfFirst { it.day == startDay }
         var day = 1
+        var workerIndex = 1
         while (true) {
             for (i in 0 until 7) {
                 val currentDay = sevenDays[(startIndex + i) % 7]
                 if (day > days) return
-                changeToHoliday(month, day, holidays, currentDay)
+                val currentWorker = totalWorkers[workerIndex - 1]
+                changeToHoliday(month, day, holidays, currentDay, currentWorker)
                 day++
+                workerIndex++
             }
         }
     }
 
-    private fun changeToHoliday(month: Int, day: Int, holidays: List<Int>, currentDay: SevenDays) {
+    private fun changeToHoliday(
+        month: Int,
+        day: Int,
+        holidays: List<Int>,
+        currentDay: SevenDays,
+        currentWorker: String
+    ) {
         if (holidays.contains(day)) {
-            return println("${month}월 ${day}일 ${currentDay.day}(휴일)")
+            return println("${month}월 ${day}일 ${currentDay.day}(휴일) $currentWorker")
         }
-        return println("${month}월 ${day}일 ${currentDay.day}")
+        return println("${month}월 ${day}일 ${currentDay.day} $currentWorker")
     }
 }
