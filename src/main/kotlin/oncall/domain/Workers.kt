c36654643184bb2fa5997a2fee53bac0b9cfbcba
박지민 <pjm5489@naver.com>
2024-11-20 18:20:30 +0900

    refactor(workers): 매개변수 순서 변경

--- src/main/kotlin/oncall/domain/Workers.kt
+++ src/main/kotlin/oncall/domain/Workers.kt
@@ -1,6 +1,6 @@
 package oncall.domain
 
-class Workers(rawHolidayWorkers: String, rawWeekdayWorkers: String) {
+class Workers(rawWeekdayWorkers: String, rawHolidayWorkers: String) {
     val weekdayWorkerNames = rawWeekdayWorkers.split(',')
     val holidayWorkerNames = rawHolidayWorkers.split(',')
 }
